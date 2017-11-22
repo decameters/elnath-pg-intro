7581c6f2d00798f47f8cc3cfd5d818e0377b8855
liz elton <eeeqqqeee@gmail.com>
2017-11-22 13:16:12 -0600

    save and delete buttons working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,16 +2,89 @@ console.log('client.js has been loaded');
 
 $(document).ready(function(){
     console.log('JQ has been loaded');
+    $('#jordansButton').on('click', addNewAirJordans)
+    $('#shoes').on('click', '.deleteButton', removeShoe); // listens for this entire section to get clicked,
+    // but only fires the removeShoe function when a .deleteButton is clicked
+    $('#shoes').on('click', '.saveButton', editShoe);
+    getAllShoes();
+})
+
+function addNewAirJordans(){
     $.ajax({
         method: 'POST',
         url: '/shoes',
         data: {
             name: 'Nike Air Jordan',
-            cost: '110'
         },
         success: function(response) {
             console.log('response', response);
-            
+            getAllShoes();
         }
     })
-})
+}
+
+function getAllShoes(){
+    $.ajax({
+        method: 'GET',
+        url: '/shoes',
+        success: function(response) {
+            // console.log('ajax get response', response);
+            $('#shoes').empty(); // .html('') would also work
+            for (let i = 0; i < response.length; i++) {
+            var shoe = response[i];
+            var $newShoeItem = $('<li>' + shoe.name +  '</li>');
+
+            // create and append the save button
+            var $saveShoeButton = $('<button class="saveButton">SAVE</button>');
+            $newShoeItem.append($saveShoeButton);
+            $saveShoeButton.data('id', shoe.id);
+    
+            // create and append the delete button
+            var $deleteShoeButton = $('<button class="deleteButton">DELETE</button>');
+            $deleteShoeButton.data('id', shoe.id);
+            $newShoeItem.append($deleteShoeButton); // appends INSIDE of the element you are referring to
+            // becomes the last element inside of the element you are referring to
+            // could put data-id= in the button HTML
+            // $('#shoes').append('<li>' + response[i].name + ', ' + response[i].cost + '<button id="delete">DELETE</button>' + '</li>')
+            
+            // append the new list item to the DOM
+            $('#shoes').append($newShoeItem);
+            }
+        }
+    })
+}
+
+function removeShoe() {
+    // console.log($(this).data());
+    
+    var shoeIdToRemove = $(this).data().id;
+    console.log('remove shoe was clicked, the shoe ID was', shoeIdToRemove);
+
+    $.ajax({
+        method: 'DELETE',
+        url: '/shoes/' + shoeIdToRemove,
+        success: function (response){
+            getAllShoes();
+        }
+    })
+}
+
+function editShoe(){
+    console.log($(this).data()); // this should log {id: '7'} or whatever the ID is
+    // save button next to delete button
+    // log the id
+    var shoeIdToSave = $(this).data().id;
+    console.log('save shoe was clicked, the shoe ID was', shoeIdToSave);
+
+    $.ajax({
+        method: 'PUT',
+        url: '/shoes/' +shoeIdToSave,
+        data: {
+            name: 'Moon Boots',
+            cost: '0'
+        },
+        success: function (response) {
+            getAllShoes();
+        }
+    })
+}
--- server/routes/shoes.js
+++ server/routes/shoes.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var pg = require('pg');
-var bodyParser = require('body-parser');
+// var pg = require('pg');
+// var bodyParser = require('body-parser');
 
 var pool = require('../modules/pool');
 
@@ -15,7 +15,7 @@ router.get( '/', function (req, res){
         } else {
             // we connected to the database!
             // we're going to GET things from the DB
-            client.query('SELECT * FROM shoes;', function(errorMakingQuery, result){ // callback function after our query runs
+            client.query('SELECT * FROM shoes ORDER BY id;', function(errorMakingQuery, result){ // callback function after our query runs
                 done();
                 if(errorMakingQuery){
                     // query failed. did you test it in Postico?
@@ -41,8 +41,63 @@ router.post('/', function(req, res){
         } else {
             // we connected to the database!
             // we're going to GET things from the DB
-            client.query(`INSERT INTO shoes (name, cost)
-            VALUES ($1, $2);`, [req.body.name, req.body.cost], function(errorMakingQuery, result){ // callback function after our query runs
+            client.query(`INSERT INTO shoes (name)
+            VALUES ($1);`, [req.body.name], function(errorMakingQuery, result){ // callback function after our query runs
+                done();
+                if(errorMakingQuery){
+                    // query failed. did you test it in Postico?
+                    // log the error
+                    console.log('Error making query', errorMakingQuery);
+                    res.sendStatus(500);
+                } else {
+                    res.sendStatus(201);
+                }
+            })
+        }
+    })
+})
+
+router.delete('/:id', function(req, res){
+    var shoeIdToRemove = req.params.id;
+    // attempt to connect to database
+    pool.connect(function (errorConnectingToDatabase, client, done){ //  this is a callback function
+        // attempt to connect to database
+        if(errorConnectingToDatabase) {
+            // there was an error connecting to the database
+            console.log('Error connecting to database', errorConnectingToDatabase);
+            res.sendStatus(500); // internal server error
+        } else {
+            // we connected to the database!
+            // we're going to GET things from the DB
+            client.query(`DELETE FROM shoes WHERE id=$1;`, [shoeIdToRemove], function(errorMakingQuery, result){ // callback function after our query runs
+                done();
+                if(errorMakingQuery){
+                    // query failed. did you test it in Postico?
+                    // log the error
+                    console.log('Error making query', errorMakingQuery);
+                    res.sendStatus(500);
+                } else {
+                    res.sendStatus(200);
+                }
+            })
+        }
+    })
+})
+
+router.put('/:id', function(req, res){
+    var shoeIdToSave = req.params.id;
+    // attempt to connect to database
+    pool.connect(function (errorConnectingToDatabase, client, done){ //  this is a callback function
+        // attempt to connect to database
+        if(errorConnectingToDatabase) {
+            // there was an error connecting to the database
+            console.log('Error connecting to database', errorConnectingToDatabase);
+            res.sendStatus(500); // internal server error
+        } else {
+            // we connected to the database!
+            // we're going to GET things from the DB
+            client.query(`UPDATE shoes SET name = $1
+            WHERE id = $2;`, [req.body.name, shoeIdToSave], function(errorMakingQuery, result){ // callback function after our query runs
                 done();
                 if(errorMakingQuery){
                     // query failed. did you test it in Postico?
